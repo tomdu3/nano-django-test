ed3a35741c592952a32fd387a3a29b9825111a23
Tomislav Dukez <tomdu3@ymail.com>
2024-11-06 12:44:09 +0100

    add rendering of the template

--- app.py
+++ app.py
@@ -3,12 +3,15 @@
 # ///
 
 from nanodjango import Django
+from django.shortcuts import render
 
 app = Django()
 
 
 @app.route('/comments')
 def comments(request):
-    return '<h1>Comments</h1>'
+    return render(request, 'index.html')
+
+
 if __name__ == "__main__":
     app.run()
