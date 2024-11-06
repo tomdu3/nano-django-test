e95361c3772a73fe1aae5727ce301816033e7213
Tomislav Dukez <tomdu3@ymail.com>
2024-11-06 12:49:44 +0100

    add model comment

--- app.py
+++ app.py
@@ -4,12 +4,22 @@
 
 from nanodjango import Django
 from django.shortcuts import render
+from django.db import models
 
 app = Django()
 
+class Comment(models.Model):
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey('auth.User', on_delete=models.CASCADE)
 
+    
 @app.route('/comments')
 def comments(request):
+    if request.method == 'POST':
+        comment = request.POST.get('comment')
+        # add it to the database
+        print(comment)
     return render(request, 'index.html')
 
 
