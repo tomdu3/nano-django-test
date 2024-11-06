67024bdf3e319b2f2d9457da5eeb2d905297a08b
Tomislav Dukez <tomdu3@ymail.com>
2024-11-06 13:13:07 +0100

    add records to db and display from db

--- app.py
+++ app.py
@@ -5,13 +5,18 @@
 from nanodjango import Django
 from django.shortcuts import render
 from django.db import models
+# from django.contrib.auth.models import User 
 
 app = Django()
 
 class Comment(models.Model):
-    comment = models.TextField()
+    '''
+    Comments model
+    '''
+    text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    # add the user
+    # created_by = models.ForeignKey(User, on_delete=models.CASCADE) 
 
     
 @app.route('/comments')
@@ -19,8 +24,12 @@ def comments(request):
     if request.method == 'POST':
         comment = request.POST.get('comment')
         # add it to the database
-        print(comment)
-    return render(request, 'index.html')
+        Comment.objects.create(text=comment)
+
+
+    comments = Comment.objects.order_by('-created_at').all()
+    context = {'comments': comments}
+    return render(request, 'index.html', context)
 
 
 if __name__ == "__main__":
