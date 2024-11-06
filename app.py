9b4df7deae004a7ef496d267d1d771958c829708
Tomislav Dukez <tomdu3@ymail.com>
2024-11-06 13:27:13 +0100

    add user model and restrictions

--- app.py
+++ app.py
@@ -5,9 +5,12 @@
 from nanodjango import Django
 from django.shortcuts import render
 from django.db import models
-# from django.contrib.auth.models import User 
+from django.contrib.auth import get_user_model
+
 
 app = Django()
+
+User = get_user_model()
 
 class Comment(models.Model):
     '''
@@ -16,18 +19,20 @@ class Comment(models.Model):
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     # add the user
-    # created_by = models.ForeignKey(User, on_delete=models.CASCADE) 
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE) 
 
-    
+
 @app.route('/comments')
 def comments(request):
     if request.method == 'POST':
         comment = request.POST.get('comment')
+        user = User.objects.first() 
         # add it to the database
-        Comment.objects.create(text=comment)
+        if comment:
+            Comment.objects.create(text=comment, created_by=user) 
 
 
-    comments = Comment.objects.order_by('-created_at').all()
+    comments = Comment.objects.filter(created_by=user).order_by('-created_at')
     context = {'comments': comments}
     return render(request, 'index.html', context)
 
